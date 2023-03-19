671f8c6d0e40655985a2bd45eb73e445ee2622c3
Claire Chabas <claire.chabas@gmail.com>
2023-03-19 18:45:06 +0100

    feat: adding components and their styles for the board columns and tasks

--- app.vue
+++ app.vue
@@ -1,5 +1,6 @@
 <template>
-  <div>
-    <NuxtWelcome />
+  <div class="p-10 h-[100vh] bg-teal-600 overflow-auto">
+    <h1 class="text-4xl text-white flex items-center mb-10">🖲️ Kanban Board</h1>
+    <Board />
   </div>
 </template>
---
+++ components/Board.vue
@@ -0,0 +1,60 @@
+<script setup lang="ts">
+import type { Column } from '~/types'
+import { nanoid } from 'nanoid'
+
+const columns = ref<Column[]>([
+  {
+    id: nanoid(),
+    title: 'Backlog',
+    tasks: [
+      {
+        id: nanoid(),
+        title: 'Create a Laravel backend for this Vue app',
+        createdAt: new Date(),
+      },
+      {
+        id: nanoid(),
+        title: 'Deploy this app',
+        createdAt: new Date(),
+      },
+    ],
+  },
+  {
+    id: nanoid(),
+    title: 'Doing',
+    tasks: [],
+  },
+  {
+    id: nanoid(),
+    title: 'Tested',
+    tasks: [],
+  },
+  {
+    id: nanoid(),
+    title: 'Deployed',
+    tasks: [],
+  },
+  {
+    id: nanoid(),
+    title: 'Done',
+    tasks: [],
+  },
+])
+</script>
+
+<template>
+  <div class="flex gap-4 overflow-x-auto items-start">
+    <div
+      v-for="column in columns"
+      :key="column.id"
+      class="bg-gray-200 p-5 rounded min-w-[250px]"
+    >
+      <header class="font-bold mb-4">{{ column.title }}</header>
+      <Task v-for="task in column.tasks" :task="task" :key="task.id" />
+
+      <footer>
+        <button class="text-gray-500">+ Add a Card</button>
+      </footer>
+    </div>
+  </div>
+</template>
