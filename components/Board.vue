f309ab29d6771e91b818219418dceb64a0edbf69
Claire Chabas <claire.chabas@gmail.com>
2023-03-19 19:22:07 +0100

    feat: styling drag and drop of a task

--- components/Board.vue
+++ components/Board.vue
@@ -70,7 +70,7 @@ const isAltKeyDown = useKeyModifier('Alt')
             item-key="id"
           >
             <template #item="{ element: task }: { element: Task }">
-              <Task :task="task" />
+              <div><Task :task="task" class="task" /></div>
             </template>
           </draggable>
 
@@ -82,3 +82,21 @@ const isAltKeyDown = useKeyModifier('Alt')
     </draggable>
   </div>
 </template>
+
+<style>
+/* Class added to an element when it's being dragged. */
+.sortable-drag .task {
+  transform: rotate(5deg);
+}
+
+/* Class added to the element underneath the element 
+* being dragged to style the spot it can be dropped it in.
+*/
+.sortable-ghost .task {
+  position: relative;
+}
+.sortable-ghost .task::after {
+  content: '';
+  @apply absolute top-0 bottom-0 right-0 left-0 bg-slate-300 rounded;
+}
+</style>
